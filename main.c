e66eb298f974ec2fe39984fac0bfc8db2d84e2d0
Jose Mendoza <joseantoniomendozanavarro@gmail.com>
2022-12-02 09:26:40 -0600

    José Mendoza fixed the player animation and movement

--- main.c
+++ main.c
@@ -20,51 +20,26 @@ int main() {
     Texture2D lanceTxtr = LoadTexture("Assets/eliteFour");
 
     RedPlayer *red = InitPlayer(screenWidth, screenHeight);
+    setPlayerTexture(redTxtr, red);
 
-    SetTargetFPS(24);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+    SetTargetFPS(24);
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    while (!WindowShouldClose())
     {
-
         float deltaTime = GetFrameTime();
         UpdatePlayer(deltaTime, red);
-        updatePlayerTexture(redTxtr, red);
-
-        // Draw
-        //----------------------------------------------------------------------------------
         BeginDrawing();
-
         ClearBackground(BLACK);
         DrawTexture(route1Txtr, 0, 0, WHITE);
-        DrawTexture(redTxtr, getPlayerPos(red).x, getPlayerPos(red).y, WHITE);
-
-        Rectangle mask;
-        mask.x = getPlayerPos(red).x;
-        mask.y = getPlayerPos(red).y * (redTxtr.height/4);
-        mask.width = redTxtr.width/3;
-        mask.height = redTxtr.height/3.6;
-        //DrawTexture(redTxtr, getPlayerPos(red).x, getPlayerPos(red).y, WHITE);
-        DrawTextureRec(redTxtr,mask, getPlayerPos(red), WHITE);
+        RenderPlayer(red, 3);
         float playerXPos = getPlayerPos(red).x;
         float playerYPos = getPlayerPos(red).y;
         DrawRectangle(screenWidth/2,15,8,8,BLUE);
         drawMapCollisionBoxes(screenWidth,screenHeight);
-        // DrawText(playerXPos, 0, 0, 8, BLACK);
-        // DrawText(playerYPos, 0, 15, 8, BLACK);
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
-
-
-
+    CloseWindow();
     return 0;
 
 }
